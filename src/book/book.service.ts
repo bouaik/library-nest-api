7f81910564d5772a54b96fe5e255ba040fb6bb09
bouaik <lhoussaine.bouaik@um6p.ma>
2023-11-13 21:29:40 +0100

    implement validation

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Book } from './schemas/book.schema';
 import mongoose from 'mongoose';
@@ -42,6 +46,12 @@ export class BookService {
   }
 
   async findById(id: string): Promise<Book> {
+    const isValidId = mongoose.isValidObjectId(id);
+
+    if (!isValidId) {
+      throw new BadRequestException('Please provide a correct Id');
+    }
+
     const book = await this.bookModel.findById(id);
 
     if (!book) {
--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,9 +1,23 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { Category } from '../schemas/book.schema';
 
 export class UpdateBookDTO {
+  @IsOptional()
+  @IsString()
   readonly title: string;
+
+  @IsOptional()
+  @IsString()
   readonly description: string;
+  @IsOptional()
+  @IsString()
   readonly author: string;
+
+  @IsOptional()
+  @IsString()
   readonly price: number;
+
+  @IsOptional()
+  @IsEnum(Category, { message: 'Please provide a correct category' })
   readonly category: Category;
 }
